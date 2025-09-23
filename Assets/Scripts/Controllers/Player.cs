6b4e6b59b94b34241783fb2a3c4a865040a2abd5
cadenasu <cadenasu@sheridancollege.ca>
2025-09-23 12:56:20 -0400

    Fixed player script

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -1,17 +1,45 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public List<Transform> asteroidTransforms;
     public Transform enemyTransform;
     public GameObject bombPrefab;
-    public Transform bombsTransform;
+    public List<Transform> asteroidTransforms;
 
+    // Update is called once per frame
     void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            SpawnBombAtOffset(new Vector3(0, 1));
+        }
+    }
+
+    void SpawnBombAtOffset(Vector3 inOffset)
+    {
+        Vector3 spawnPosition = transform.position + inOffset;
+        Instantiate(bombPrefab, spawnPosition, Quaternion.identity);
+    }
+
+    public void SpawnBombTrail(float inBombSpacing, int inNumberOfBombs)
     {
 
     }
 
-}
+    public void SpawnBombOnRandomCorner(float inDistance)
+    {
+
+    }
+
+    public void WarpPlayer(Transform target, float ratio)
+    {
+
+    }
+
+    public void DetectAsteroids(float inMaxRange, List<Transform> inAsteroids)
+    {
+
+    }
+
+}    
