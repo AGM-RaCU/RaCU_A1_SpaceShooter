4347bc5ccb5c143258c59a439f7dfa0e37be069b
cadenasu <cadenasu@sheridancollege.ca>
2025-09-23 15:02:10 -0400

    Inclass exercise complete

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -6,6 +6,13 @@ public class Player : MonoBehaviour
     public Transform enemyTransform;
     public GameObject bombPrefab;
     public List<Transform> asteroidTransforms;
+    public Transform bombsTransform;
+
+    //[Header("Motion Properties")]
+
+    [Header("Radar Properties")]
+    public float greenCircleRadius = 1f;
+    public int numberOfSides = 6;
 
     // Update is called once per frame
     void Update()
@@ -14,6 +21,14 @@ public class Player : MonoBehaviour
         {
             SpawnBombAtOffset(new Vector3(0, 1));
         }
+
+        //PlayerMovement();
+        PlayerRadar(greenCircleRadius, numberOfSides);
+    }
+
+    void PlayerMovement()
+    {
+        
     }
 
     void SpawnBombAtOffset(Vector3 inOffset)
@@ -40,6 +55,29 @@ public class Player : MonoBehaviour
     public void DetectAsteroids(float inMaxRange, List<Transform> inAsteroids)
     {
 
+    }
+
+    public void PlayerRadar(float greenCircleRadius, int numberOfSides)
+    {
+        float angleStep = 360f / numberOfSides;
+        float radians = angleStep * Mathf.Deg2Rad;
+
+        List<Vector3> points = new List<Vector3>();
+        for (int i = 0; i < numberOfSides; i++)
+        {
+
+            float adjustment = radians * i;
+            Vector3 point = new Vector3(Mathf.Cos(radians + adjustment), Mathf.Sin(radians + adjustment)) * greenCircleRadius;
+            points.Add(point);
+        }
+
+        Vector3 center = transform.position;
+
+        for (int i = 0; i < points.Count - 1; i++)
+        {
+            Debug.DrawLine(center + points[i], center +  points[i + 1], Color.green);
+        }
+        Debug.DrawLine(center + points[points.Count - 1], center + points[0], Color.green);
     }
 
 }    
